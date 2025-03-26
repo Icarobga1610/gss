c8faffe1efb7fa227716ef2e0db48886d468a9e5
Icarobga1610 <163910242+Icarobga1610@users.noreply.github.com>
2025-03-26 00:21:17 -0400

    Add footer to index.html and handle 404 errors in app.py

--- projeto_maot_gss/app.py
+++ projeto_maot_gss/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
-import webbrowser
 import threading
+import webbrowser
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def abrir_navegador():
     webbrowser.open_new('http://127.0.0.1:5050')
